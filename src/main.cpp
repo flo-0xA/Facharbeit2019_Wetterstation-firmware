51856b1e3fd3d0fabb9dfb53832d628cbda6e545
Florian Abeln <fabeln06@gmail.com>
2019-08-09 16:24:32 +0200

    Regensensor implementiert. Fehler behoben.

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@
 
 /* Variablen im Deppsleep beibehalten */
 RTC_DATA_ATTR int bootCount = 0;
-RTC_DATA_ATTR int waterTippingCount = 0;
+RTC_DATA_ATTR float rainValue = 0.272727273F;
 
 /* Konfiguration für MQTT - Kommunikation */
 const char* SSID = "IoTify";
@@ -31,6 +31,7 @@ const char* topic_humidity = "/wetterstation/humidity";
 const char* topic_pressure = "/wetterstation/pressure";
 const char* topic_windSpeed = "/wetterstation/wind_speed";
 const char* topic_windDirection = "/wetterstation/wind_direction";
+const char* topic_rain = "/wetterstation/rain";
 
 /* Definitionen bzw. Deklaration für den Windsensor  */
 const char* directionTable[] = {"N","NNE","NE","ENE","E","ESE","SE","SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"};
@@ -80,14 +81,16 @@ void reconnect() {
         }
     }
 }
+
+int wakeup_reason() {
+    return esp_sleep_get_wakeup_cause();
+}
  
 void setup() {
     Serial.begin(115200);
 
     ++bootCount;
     Serial.println("Anzahl der Boots / Starts: " + String(bootCount));
-
-    wakeup_reason();
 
     esp_sleep_enable_ext0_wakeup(GPIO_NUM_33, 1);   // Auf Regensensor reagieren
     esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);  // Auf RTC reagieren
@@ -113,19 +116,24 @@ void setup() {
     }
     client.loop();
 
-    /* Temperatursensor auslesen */
-    temperature = tempSensor.readTemperature();
-    humidity = tempSensor.readHumidity();
-    pressure = tempSensor.readPressure() / 100.0F;
+    if (wakeup_reason() == ESP_SLEEP_WAKEUP_TIMER) {
+        /* Temperatursensor auslesen */
+        temperature = tempSensor.readTemperature();
+        humidity = tempSensor.readHumidity();
+        pressure = tempSensor.readPressure() / 100.0F;
 
-    client.publish(topic_temperature, String(temperature).c_str());
-    client.publish(topic_humidity, String(humidity).c_str());
-    client.publish(topic_pressure, String(pressure).c_str());
+        client.publish(topic_temperature, String(temperature).c_str());
+        client.publish(topic_humidity, String(humidity).c_str());
+        client.publish(topic_pressure, String(pressure).c_str());
 
-    /* Windsensor bzw. Anemometer auslesen */
-    if(windSensor.read(speed, direction)) {
-        client.publish(topic_windSpeed, String(speed, 1).c_str());
-        client.publish(topic_windDirection, directionTable[direction]);
+        /* Windsensor bzw. Anemometer auslesen */
+        if(windSensor.read(speed, direction)) {
+            client.publish(topic_windSpeed, String(speed, 1).c_str());
+            client.publish(topic_windDirection, directionTable[direction]);
+        }
+    } else {
+        rainValue += 0.272727273F;
+        client.publish(topic_rain, String(rainValue).c_str());
     }
 
     delay(5000);
@@ -137,11 +145,7 @@ void setup() {
     client.disconnect();
     WiFi.disconnect();
 
-    esp_deep_sleep_start(); // für 60 Sekunden schlafen
-}
-
-int wakeup_reason() {
-    return esp_sleep_get_wakeup_cause();
+    esp_deep_sleep_start();
 }
 
 void loop() {}
