0fe6f4a2c7ee53aa16787cd2101b5513bd6488c7
Florian Abeln <fabeln06@gmail.com>
2019-08-18 16:34:50 +0200

    Deepsleep implementiert

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,12 @@
 // #include <Adafruit_BME280.h>
 // #include <LaCrosse_TX23.h>
 
+/* Konfiguartion für Deepsleep */
+#define uS_TO_S_FACTOR 1000000
+#define TIME_TO_SLEEP 60
+
+RTC_DATA_ATTR int bootCount = 0;
+
 void setup()
 {
     Serial.begin(115200);
@@ -24,6 +30,11 @@ void setup()
     ota::exec();
 
     // 2. Deepsleep konfigurieren
+    bootCount++;
+    Serial.println("Anzahl der Boots / Starts: " + String(bootCount));
+
+    esp_sleep_enable_ext0_wakeup(GPIO_NUM_33,1);  // Auf Regensensor reagieren
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);  // Auf RTC reagieren
 
 
     // 3. Verbindung zu MQTT-Broker herstellen
@@ -33,6 +44,13 @@ void setup()
 
 
     // 5. Deepsleep ausführen
+    Serial.println("ESP wird in Deepsleep versetzt...");
+    Serial.flush();
+
+    // mqttClient.disconnect();
+    WiFi.disconnect();
+
+    esp_deep_sleep_start();
 
 }
 
