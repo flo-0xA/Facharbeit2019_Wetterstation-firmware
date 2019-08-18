3f512c29d0fdf82de02b396608ac8f8218da2832
Florian Abeln <fabeln06@gmail.com>
2019-08-18 13:50:35 +0200

    Definitionen aller Varieblen sind nun global

---
+++ src/GlobalDefinitions.cpp
@@ -0,0 +1,24 @@
+/*
+ *  Hier werden alle Konstanten global definiert.
+ */
+#include "GlobalDefinitions.hpp"
+
+/* Dauer (in Sekunden) des Deepsleeps */
+short TIME_TO_SLEEP = 60;
+
+/* Konfiguratione der WLAN-Verbindung */
+char* WLAN_SSID = "IoTify";
+char* WLAN_KEY = "wBox2019";
+
+char* OTA_FIRMWARE_PATH = "";
+
+/* Konfiguration für MQTT */
+char* MQTT_BROKER = "192.168.178.46";
+
+char* TOPIC_TEMPERATURE = "/wetterstation/temperature";
+char* TOPIC_HUMIDITY = "/wetterstation/humidity";
+char* TOPIC_PRESSURE = "/wetterstation/pressure";
+char* TOPIC_WIND_SPEED = "/wetterstation/wind_speed";
+char* TOPIC_WIND_DIRECTION = "/wetterstation/wind_direction";
+char* TOPIC_RAIN = "/wetterstation/rain";
+char* TOPIC_BATTERY = "/wetterstation/battery";
---
+++ src/GlobalDefinitions.hpp
@@ -0,0 +1,22 @@
+#ifndef _GLOBALDEFINITIONS_HPP_
+
+#define _GLOBALDEFINITIONS_HPP_
+
+extern short TIME_TO_SLEEP;
+
+extern char* WLAN_SSID;
+extern char* WLAN_KEY;
+
+extern char* OTA_FIRMWARE_PATH;
+
+extern char* MQTT_BROKER;
+
+extern char* TOPIC_TEMPERATURE;
+extern char* TOPIC_HUMIDITY;
+extern char* TOPIC_PRESSURE;
+extern char* TOPIC_WIND_SPEED;
+extern char* TOPIC_WIND_DIRECTION;
+extern char* TOPIC_RAIN;
+extern char* TOPIC_BATTERY;
+
+#endif
