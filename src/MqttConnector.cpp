c8e7f676a25e7881b7e3696922fd908a34cb47fa
Florian Abeln <fabeln06@gmail.com>
2019-08-19 08:42:12 +0200

    MQTT-Wraper implementiert

---
+++ src/MqttConnector.cpp
@@ -0,0 +1,55 @@
+/*
+ * 
+ */
+#include "MqttConnector.hpp"
+#include "GlobalDefinitions.hpp"
+#include "WlanConnector.hpp"
+
+#include <PubSubClient.h>
+
+namespace mqtt
+{
+    PubSubClient client(wlan::getClient());
+
+    /*
+     * 
+     */
+    void connect()
+    {
+        client.setServer(MQTT_HOST,  MQTT_PORT);
+
+        Serial.println("Mit " + String(MQTT_HOST) + "-Broker verbinden");
+
+        if (!client.connect("ESP_Weatherstation"))
+        {
+            delay(500);
+            Serial.print(".");
+        }
+
+        Serial.println("Mit Broker: " + String(MQTT_HOST) + " erfolgreich verbunden");
+    }
+
+    /*
+     * 
+     */
+    void disconnect()
+    {
+        client.disconnect();
+    }
+
+    /*
+     * 
+     */
+    bool isConnected()
+    {
+        return client.connected();
+    }
+
+    /*
+     *
+     */
+    bool publish(char* topic, char* message)
+    {
+        client.publish(topic, message);
+    }
+}
