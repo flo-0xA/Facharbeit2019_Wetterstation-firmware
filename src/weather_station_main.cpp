6e4657701a21ba9c16afb187f63ab042e6723ec9
Florian <fabeln06@gmail.com>
2020-07-05 16:15:31 +0200

    added constants and variables

--- src/weather_station_main.cpp
+++ src/weather_station_main.cpp
@@ -1,11 +1,36 @@
 #include <Arduino.h>
+#include "initialization.h"
+
+#define SENSOR_WIND 18
+
+#define SEA_LEVEL_PREPRESSURE 1013.25
+#define RAIN_LEVEL 0.272727273
+
+#define TOPIC_TEMPERATURE ""
+#define TOPIC_HUMIDITY ""
+#define TOPIC_PRESSURE ""
+#define TOPIC_WIND_SPEED ""
+#define TOPIC_WIND_DIRECTION ""
+#define TOPIC_RAIN ""
+
+
+RTC_DATA_ATTR uint8_t boot_count = 0;
+
+
+const char* direction_table[] = {"N","NNE","NE","ENE","E","ESE","SE","SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"};
+int direction;
+float speed;
+ 
+bool status;
+float temperature;
+float humidity;
+float pressure;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  sleep(100);
+  deepsleep_init();
+
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  sleep(100);
-}
+void loop() {}
