0aac97b9ab51f7f1fcb3a188e8730e65eaae5efd
Florian <fabeln06@gmail.com>
2020-07-05 19:47:31 +0200

    added documentation of sourcecode

--- src/initialization.cpp
+++ src/initialization.cpp
@@ -1,14 +1,15 @@
 #include "WiFi.h"
 #include "PubSubClient.h"
 
-
+/*
+** Anzahl der Verbindungsversuche, die durchgeführt werden sollen, bis der Vorganga abgebrochen wird
+*/
 #define CONNECTION_ATTEMPTS 10
 
 #define WIFI_SSID "AP-test"
 #define WIFI_KEY "2124234431346"
 
 #define MQTT_BROKER "hostname"
-#define MQTT_PWD ""
 #define MQTT_PORT 1884
 
 #define UART_BAUDRATE 11520
@@ -22,17 +23,28 @@ WiFiClient wifi_client;
 PubSubClient mqtt_client(wifi_client);
 
 
+/*
+** ESP-Deepsleep mit deffiniertem Interruptpin initialisieren
+**
+** Der ESP wacht auf, wenn Pin 33 auf LOW gezogen wird.
+*/
 void deepsleep_init()
 {
     esp_sleep_enable_ext0_wakeup(GPIO_NUM_33,1);
 }
 
+/*
+** Serielle Schnittstelle mit der definierten Baudrate öffnen
+*/
 bool interface_init()
 {
     Serial.begin(UART_BAUDRATE);
     Serial.println("INFO: Serielle Schnittstelle gestartet...");
 }
 
+/*
+** Verbindung mit dem WLAN-Accesspoint herstellen
+*/
 bool wifi_init()
 {
     WiFi.begin(WIFI_SSID, WIFI_KEY);
@@ -51,6 +63,9 @@ bool wifi_init()
     return false;
 }
 
+/*
+** Verbindung mit dem MQTT Broker herstellen
+*/
 bool mqtt_init()
 {
     Serial.printf("INFO: Verbinden mit %s Broker ...", MQTT_BROKER);
