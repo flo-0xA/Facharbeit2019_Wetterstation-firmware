fad4e9208383b1aed70caf97284644162a933dbd
Florian <fabeln06@gmail.com>
2020-07-05 15:57:34 +0200

    added new init function

--- include/initialization.h
+++ include/initialization.h
@@ -1,6 +1,10 @@
-#ifndef INITIALIZATION
-#define INITIALIZATION
+#ifndef INITIALIZATION_H
 
+#define INITIALIZATION_H
 
+void deepsleep_init();
+bool interface_init();
+bool wifi_init();
+bool mqtt_init();
 
 #endif
