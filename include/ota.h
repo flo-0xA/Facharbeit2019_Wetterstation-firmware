9c3e3ddf2e03840949fef50c32ad541cf4773c09
Florian Abeln <fabeln06@gmail.com>
2020-08-26 22:20:33 +0200

    Initial commit. Rewriting

---
+++ include/ota.h
@@ -0,0 +1,6 @@
+#ifndef __OTA_H__
+#define __OTA_H__
+
+
+
+#endif
---
+++ include/setup.h
@@ -0,0 +1,25 @@
+#ifndef __SETUP_H__
+#define __SETUP_H__
+
+#define TOPIC_TEMPERATURE "/weatherstation/temperature"
+#define TOPIC_HUMIDITY "/weatherstation/humidity"
+#define TOPIC_PRESSURE "/weatherstation/pressure"
+#define TOPIC_UVA "/weatherstation/uv/a"
+#define TOPIC_UVB "/weatherstation/uv/b"
+#define TOPIC_UV_INDEX "/weatherstation/uv/index"
+#define TOPIC_WIND_SPEED "/weatherstation/wind/speed"
+#define TOPIC_RAIN "/weatherstation/rain"
+#define TOPIC_BATTERY "/weatherstation/battery"
+#define TOPIC_ERROR "/weatherstation/error"
+
+#define HOSTNAME "wetter-station"
+
+#define STATION "wlan-ap"
+#define KEY "123456789"
+
+#define MQTT_SERVER "mqtt-broker"
+#define MQTT_PORT 12345
+#define MQTT_USER ""
+#define MQTT_PASSWORD ""
+
+#endif
