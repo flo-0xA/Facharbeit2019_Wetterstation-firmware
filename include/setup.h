63f7f18c7ce4edece621e193215ed8a3cd6edcda
Florian <fabeln06@gmail.com>
2020-09-07 19:49:47 +0200

    updated

--- include/setup.h
+++ include/setup.h
@@ -1,13 +1,6 @@
 #ifndef __SETUP_H__
 #define __SETUP_H__
 
-/*
- * OTA-Modus definieren
- *  0 = Update wird von einem Server geladen
- *  1 = Update über Webportal
- */
-#define OTA_MODE 0
-
 #define TOPIC_TEMPERATURE "/weatherstation/temperature"
 #define TOPIC_HUMIDITY "/weatherstation/humidity"
 #define TOPIC_PRESSURE "/weatherstation/pressure"
@@ -21,15 +14,14 @@
 
 #define HOSTNAME "wetter-station"
 
-#define OTA_SERVER "ota-server"
-#define OTA_PORT 12345
+#define OTA_PORT 3232
 #define OTA_PASSWORD "123456789"
 
-#define STATION "wlan-ap"
-#define KEY "123456789"
+#define STATION "AP-HFG54JH3"
+#define KEY "0854700482640451"
 
-#define MQTT_SERVER "mqtt-broker"
-#define MQTT_PORT 12345
+#define MQTT_SERVER "192.168.178.41"
+#define MQTT_PORT 1883
 #define MQTT_USER ""
 #define MQTT_PASSWORD ""
 
